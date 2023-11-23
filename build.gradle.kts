3b21d9006ae4931656f65c9f445e434e556baf79
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-11-23 20:34:43 +0700

    add ui setting & my profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.4")
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
