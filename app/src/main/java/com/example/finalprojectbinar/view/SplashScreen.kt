673b4065a350fe4ee31b9a6def22d0e31c3e8801
hasbihz24 <hasbihz24@gmail.com>
2023-11-24 19:31:10 +0700

    Fix Gradle Properties enableJetifier

---
+++ app/src/main/java/com/example/finalprojectbinar/view/SplashScreen.kt
@@ -0,0 +1,4 @@
+package com.example.finalprojectbinar.view
+
+class SplashScreen {
+}
---
+++ app/src/main/java/com/example/finalprojectbinar/view/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalprojectbinar.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalprojectbinar.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
