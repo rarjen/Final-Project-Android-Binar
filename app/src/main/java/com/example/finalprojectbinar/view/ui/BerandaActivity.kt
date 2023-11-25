b894015a2998ec3a2131dfe361d83b3d2f241427
Kavarera <rafli.030715@gmail.com>
2023-11-25 11:00:17 +0700

    add add BerandaActivity

---
+++ app/src/main/java/com/example/finalprojectbinar/view/ui/BerandaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalprojectbinar.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalprojectbinar.R
+
+class BerandaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda)
+    }
+}
