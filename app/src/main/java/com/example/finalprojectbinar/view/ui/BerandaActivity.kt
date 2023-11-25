9233ca9d817d83b6df430d6d6dafe00528bf0114
Kavarera <rafli.030715@gmail.com>
2023-11-25 15:31:58 +0700

    beranda ui

--- app/src/main/java/com/example/finalprojectbinar/view/ui/BerandaActivity.kt
+++ app/src/main/java/com/example/finalprojectbinar/view/ui/BerandaActivity.kt
@@ -3,10 +3,17 @@ package com.example.finalprojectbinar.view.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.finalprojectbinar.R
+import com.example.finalprojectbinar.databinding.ActivityBerandaBinding
+import com.example.finalprojectbinar.view.adapters.TabLayoutKursusPopulerAdapter
 
 class BerandaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBerandaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_beranda)
+        binding = ActivityBerandaBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
+
 }
