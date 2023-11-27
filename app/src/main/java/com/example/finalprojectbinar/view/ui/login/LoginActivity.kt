a4353759d5deecfec7c2dad677896d95ab3aa47c
fauzan <muhammadalfauzan254@gmail.com>
2023-11-27 20:03:49 +0700

    ui auth

---
+++ app/src/main/java/com/example/finalprojectbinar/view/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalprojectbinar.view.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalprojectbinar.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/finalprojectbinar/view/ui/register/VerifyPhoneActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalprojectbinar.view.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalprojectbinar.R
+
+class VerifyPhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_phone)
+    }
+}
