5bb40fd06b6ca2b137c376be829b33fc8dda3c8b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-11-29 08:32:02 +0700

    setup di koin

---
+++ app/src/main/java/com/example/finalprojectbinar/di/KoinModule.kt
@@ -0,0 +1,15 @@
+package com.example.finalprojectbinar.di
+
+import org.koin.dsl.module
+
+object KoinModule {
+    val dataModule
+        get() = module {
+
+        }
+
+    val uiModule
+        get() = module {
+
+        }
+}
