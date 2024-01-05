ab74d678b3c249c1e9b2a59b3fdd2eec756ded73
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-01-05 13:02:19 +0700

    fix payment

--- app/src/main/java/com/example/finalprojectbinar/view/fragments/payment/CreditCardFragment.kt
+++ app/src/main/java/com/example/finalprojectbinar/view/fragments/payment/CreditCardFragment.kt
@@ -1,23 +1,46 @@
 package com.example.finalprojectbinar.view.fragments.payment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.finalprojectbinar.R
+import com.example.finalprojectbinar.databinding.FragmentBankBinding
+import com.example.finalprojectbinar.databinding.FragmentCreditCardBinding
 
 class CreditCardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
-    override fun onCreateView(
+    private var _binding: FragmentCreditCardBinding? = null
+    private val binding get() = _binding!!
+
+        override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
+
+        _binding = FragmentCreditCardBinding.inflate(inflater, container, false)
+
+//        binding.apply {
+//
+//            if (creditCardEt.text.isBlank() || cardHolderEt.text.isBlank() || cvvEt.text.isBlank() || expiryDateEt.text.isBlank()) {
+//                Toast.makeText(requireContext(), "Field tidak boleh kosong", Toast.LENGTH_SHORT).show()
+//            } else {
+//                // Melakukan payment
+//            }
+//        }
+
         activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
-        return inflater.inflate(R.layout.fragment_credit_card, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
